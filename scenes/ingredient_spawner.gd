bd88a05427a50ef5b578ed28b315f6b05c7e2e82
Jeffrey Bee <jeffreypbee@gmail.com>
2024-08-19 14:00:01 -0400

    Added ingredient sprite to ingredient spawner

--- scenes/ingredient_spawner.gd
+++ scenes/ingredient_spawner.gd
@@ -10,7 +10,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if ingredient :
+		if $IngredientSprite.texture != ingredient.sprite :
+			$IngredientSprite.texture = ingredient.sprite
 
 
 func spawn_ingredient() -> void:
